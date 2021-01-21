8f50e48e4fbca35b235241a2685fd4c609a7ba95
unknown <flor20_05@hotmail.com>
2021-01-21 17:23:55 -0300

    initial setup

---
+++ rodados-ego/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './components/home/home.component'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ rodados-ego/src/components/home/home.component.jsx
@@ -0,0 +1,15 @@
+
+
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>chau home</h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
